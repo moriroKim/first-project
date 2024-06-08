e5e82f41710379d041e169a41e4a3a6363291bf7
moriroKim <162679625+moriroKim@users.noreply.github.com>
2024-06-08 16:03:50 +0900

    Add files via upload

--- Calculator/ani.js
+++ Calculator/ani.js
@@ -17,3 +17,15 @@ element.addEventListener("click", function(e) {
   // -> and re-adding the class
   element.classList.add("bounce");
 }, false);
+
+//현재 시간 기능
+const clock = document.getElementById('time');
+
+setInterval(() => {
+    let date = new Date();
+    let hour = date.getHours().toString().padStart(2, '0');
+    let minutes = date.getMinutes().toString().padStart(2, '0');
+    let second = date.getSeconds().toString().padStart(2, '0');
+
+    clock.textContent = `${hour}:${minutes}:${second}`;
+}, 1000);
